1b335b56ac220f50181b7d07057668b227d267f0
jmmontes13 <68789231+jmmontes13@users.noreply.github.com>
2020-10-21 12:30:12 -0300

    Update align2D.py

--- align2D.py
+++ align2D.py
@@ -1,14 +1,11 @@
 from modeller import *
-log.verbose()
-env = environ()
-env.io.atom_files_directory = ['./']
-aln = alignment(env)
-mdl = model(env)
-# Read the whole 4eam atom file
-mdl.read(file='4eam.pdb', model_segment=('FIRST:@', 'END:'))
 
-aln.append_model(mdl, align_codes='4eam', atom_files="4eam.pdb")
+env = environ()
+aln = alignment(env)
+mdl = model(env, file='4eam', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='4eam', atom_files='4eam.pdb')
 aln.append(file='target.fasta', align_codes='target', alignment_format='FASTA')
 aln.align2d()
-aln.write(file='align.ali', alignment_format='PIR')
-aln.write(file='align.pap', alignment_format='PAP')
+aln.write(file='aligned.fasta', alignment_format='FASTA')
+aln.write(file='aligned.ali', alignment_format='PIR')
+aln.write(file='aligned.pap', alignment_format='PAP')
