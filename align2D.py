e9f68d4030d3281e54f5bc3adc447d26b8bd92c8
jmmontes13 <68789231+jmmontes13@users.noreply.github.com>
2020-10-21 12:04:28 -0300

    Add files via upload

---
+++ align2D.py
@@ -0,0 +1,11 @@
+from modeller import *
+
+env = environ()
+aln = alignment(env)
+mdl = model(env, file='4eam', model_segment=('FIRST:A','LAST:A'))
+aln.append_model(mdl, align_codes='4eam', atom_files='4eam.pdb')
+aln.append(file='target.fasta', align_codes='target', alignment_format='FASTA')
+aln.align2d()
+aln.write(file='aligned.fasta', alignment_format='FASTA')
+aln.write(file='aligned.ali', alignment_format='PIR')
+aln.write(file='aligned.pap', alignment_format='PAP')
