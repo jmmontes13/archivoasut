be206d6039067a8ae5f807b54adb480a1db07d33
jmmontes13 <68789231+jmmontes13@users.noreply.github.com>
2020-10-21 20:51:28 -0300

    Update model-single.py

--- model-single.py
+++ model-single.py
@@ -19,6 +19,14 @@ ok_models = filter(lambda x: x['failure'] is None, a.outputs)
 key = 'DOPE score'
 ok_models.sort(lambda a,b: cmp(a[key], b[key]))
 
-# Get top model
+# Get top models
 m = ok_models[0]
 print "Top model: %s (DOPE score %.3f)" % (m['name'], m[key])
+m2 = ok_models[1]
+print "Top model: %s (DOPE score %.3f)" % (m2['name'], m2[key])
+m3 = ok_models[2]
+print "Top model: %s (DOPE score %.3f)" % (m3['name'], m3[key])
+m4 = ok_models[3]
+print "Top model: %s (DOPE score %.3f)" % (m4['name'], m4[key])
+m5 = ok_models[4]
+print "Top model: %s (DOPE score %.3f)" % (m5['name'], m5[key])
