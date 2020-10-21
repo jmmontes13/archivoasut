393777e26a14226749791d2552ece6b1019c03dc
jmmontes13 <68789231+jmmontes13@users.noreply.github.com>
2020-10-21 12:37:38 -0300

    Update align2DAA.py

--- align2DAA.py
+++ align2DAA.py
@@ -4,7 +4,7 @@ from modeller import *
 env = environ()
 aln = alignment(env)
 mdl = model(env, file='4eam.pdb', model_segment=('FIRST:A','LAST:A'))
-aln.append_model(mdl, align_codes='4eam', atom_files='4eam.pdb')
+aln.append_model(mdl, align_codes='4EAM', atom_files='4eam.pdb')
 aln.append(file='target.fasta', align_codes='target', alignment_format='FASTA')
 aln.align2d()
 aln.write(file='aligned.fasta', alignment_format='FASTA')
